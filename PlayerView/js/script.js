7f44e8299bce441b18ff6805e38c34354348a14c
Pedro Sobral <80040598+pSbral@users.noreply.github.com>
2024-03-15 20:12:47 -0300

    layout changes
    layout changes

---
+++ PlayerView/js/script.js
@@ -0,0 +1,10 @@
+var ProgressBar = require('progressbar.js')
+
+var bar = new ProgressBar.Circle(container, {
+    strokeWidth: 6,
+    easing: 'easeInOut',
+    duration: 1400,
+    color: '#FFEA82',
+    trailColor: '#eee',
+    trailWidth: 1,
+});
